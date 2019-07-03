6619aa90dd26c02e3c4da83c17d818a6c702d857
Rutvij <44467789+RutvijBhutaiya@users.noreply.github.com>
2019-07-03 15:17:48 +0530

    Predicted New_Price

--- Used Car Price.R
+++ Used Car Price.R
@@ -6,18 +6,130 @@ attach(train)
 
 str(train)    ## Structure if the dataset
 
-train$Power = as.numeric(as.character(Power))  ## Convert Power var into Numeric var
+train$Power = as.numeric(as.character(Power)) ## Convert Power var into Numeric var
 
 colSums(is.na(train))   ## Check for NAs
 
-library(rpivotTable)
-rpivotTable(train)
 
-## NOTES : 
-## 1. Year -  Merge 98,99,00
-## 2. Seats -  Remove 0
-## 3. Predict New_Proce based on Proice..ANd then use model on test data
-## 4. Can we Pridict 500 then all together 100 then 1500 .. like for test NEw_Price pred.
+#library(rpivotTable)
+#rpivotTable(train)
+
+train = train[-4000,]   #3 Remove Zero Seat obersavation - Makes No sence
+
+attach(train)
+
+Predict_New_Test = train
+
+## Ommit NAs
+
+train_new = na.omit(train)
+
+colSums(is.na(train_new))
+
+summary(train_new)
+str(train_new)
+
+attach(train_new)
 
 
-train = na.omit(train)
+
+## Check Target Variable Significant Level 
+
+chisq.test(New_Price, Name)
+chisq.test(New_Price, Location)
+chisq.test(New_Price, Fuel_Type)
+chisq.test(New_Price, Transmission)
+chisq.test(New_Price, Owner_Type)
+
+## NORMALIZE DATASET - ONLY VARS - Power,  Engine, Kilometer_Driven
+
+library(MASS)
+library(forecast)
+
+hist(Power)  ## Power
+
+BoxCox.lambda(Power)
+Power = 1/ Power
+Power = sqrt(Power)
+
+
+hist(Engine)  ## Engine
+
+BoxCox.lambda(Engine)
+Engine = 1 / Engine
+Engine = sqrt(Engine)
+
+hist(Kilometers_Driven)  # Kilometer_Driven
+
+BoxCox.lambda(Kilometers_Driven)
+Kilometers_Driven = log(Kilometers_Driven)
+Kilometers_Driven = Kilometers_Driven^2
+
+attach(train_new)
+
+library(ggplot2)
+library(ggcorrplot)
+
+corp = train_new[, -c(1,2,3,5,6,7)]
+
+ggcorrplot(cor(corp), method = 'circle', type = 'lower')
+
+## Due to High Correlation between independent Vraiable - Multicolinarity 
+## Use CART
+
+
+library(rpart)
+library(rpart.plot)
+library(rattle)
+
+
+m.ctrl = rpart.control(minsplit = 90, minbucket = 10, cp = 0, xval = 10)
+
+model1 = rpart(formula = New_Price ~ ., data = train_new, method = 'anova', 
+               control = m.ctrl)
+
+fancyRpartPlot(model1)
+
+printcp(model1)
+
+model1.prun = prune(model1, cp = 0.000429, 'cp')
+fancyRpartPlot(model1.prun)
+
+par(mfrow = c(1,2))
+rsq.rpart(model1.prun)
+
+
+## Create Dataset Predict_New_Test for New_Price Predict Empty Dataset 
+
+## 1st Convert NA - train$New_Price into 0 
+## 2nd Creat dataset accordingly train$New_Price ==0
+## 3rd Load subset - Predict_New_Price
+
+
+
+library(imputeTS)
+
+Predict_New_Test$New_Price = na.replace(Predict_New_Test$New_Price,0)
+
+Predict_New_Test = Predict_New_Test[which(Predict_New_Test$New_Price == 0),]
+
+colSums(is.na(Predict_New_Test))
+
+Predict_New_Test = na.omit(Predict_New_Test)
+
+Predict_New_Test$New_Price = predict(model1.prun, Predict_New_Test, method = 'anova',  interval = 'confident')
+
+## Final Dataset 
+
+train_final = rbind(train_new, Predict_New_Test)
+
+write.csv(train_final, 'train_final.csv')
+
+
+
+
+
+
+
+
+
